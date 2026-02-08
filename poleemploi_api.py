d2c109e1adda6b295f24a358c7c34b325f74348b
BOUCHERET <webmasterdu63@gmail.com>
2026-02-08 19:45:29 +0100

    Update poleemploi_api.py

--- poleemploi_api.py
+++ poleemploi_api.py
@@ -2,10 +2,12 @@
 def search_pole_emploi(job, location, contract):
     return [
         {
-            "source": "Pôle Emploi",
-            "title": f"{job} - Pôle Emploi",
+            "source": "France Travail",
+            "title": f"{job} - France Travail",
             "location": location,
             "contract": contract,
-            "url": "https://pole-emploi.fr"
+            "url": "https://francetravail.fr.fr"
         }
     ]
+CLIENT_ID = "PAR_itjobfinder2026_9e348be54947551c54dcb05b0c802b95848270cb80f662d9f6cd4226927b8dbd"
+CLIENT_SECRET = "74ab1b5fa26760ab13042b7eff027bb21e4b55fd18f93e832f461de69743c88a"
