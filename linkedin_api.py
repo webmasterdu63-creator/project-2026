14c9b4599f0ed2ad88d9f48af83984421c5e2a1e
BOUCHERET <webmasterdu63@gmail.com>
2026-02-08 19:00:56 +0100

    Update linkedin_api.py

--- linkedin_api.py
+++ linkedin_api.py
@@ -1,1 +1,11 @@
+def search_linkedin(job, location, contract):
+    return [
+        {
+            "source": "LinkedIn",
+            "title": f"{job} - LinkedIn",
+            "location": location,
+            "contract": contract,
+            "url": "https://linkedin.com"
+        }
+    ]
 
