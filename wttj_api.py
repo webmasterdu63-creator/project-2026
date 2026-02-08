124daeef657247416d3536097b82d1bf0850e774
BOUCHERET <webmasterdu63@gmail.com>
2026-02-08 19:01:58 +0100

    Update wttj_api.py

--- wttj_api.py
+++ wttj_api.py
@@ -1,1 +1,11 @@
+def search_wttj(job, location, contract):
+    return [
+        {
+            "source": "Welcome To The Jungle",
+            "title": f"{job} - WTTJ",
+            "location": location,
+            "contract": contract,
+            "url": "https://www.welcometothejungle.com"
+        }
+    ]
 
