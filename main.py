61f424693701ece27104a819c2de4033492d1dd2
BOUCHERET <webmasterdu63@gmail.com>
2026-02-08 17:56:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from PySide6.QtWidgets import QApplication
 from ui.main_window import MainWindow
+from ui.splash_screen import SplashScreen
 import sys
+import time
 
 def main():
     app = QApplication(sys.argv)
 
+    # Splash screen
+    splash = SplashScreen()
+    splash.show()
+
+    # Simulation du chargement (API, config, etc.)
+    time.sleep(2)
+
+    # Lancement de la fenêtre principale
     window = MainWindow()
     window.show()
+
+    splash.finish(window)
 
     sys.exit(app.exec())
 
