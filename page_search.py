4a0c0517a91283666e3fbadf2c6bff6f72bfb621
BOUCHERET <webmasterdu63@gmail.com>
2026-02-08 18:33:29 +0100

    Update page_search.py

--- page_search.py
+++ page_search.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
+from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QLineEdit, QPushButton
 from PySide6.QtCore import Qt
 
 class PageSearch(QWidget):
@@ -6,6 +6,18 @@ class PageSearch(QWidget):
         super().__init__()
 
         layout = QVBoxLayout(self)
-        label = QLabel("Page de recherche")
-        label.setAlignment(Qt.AlignCenter)
-        layout.addWidget(label)
+
+        title = QLabel("Recherche d'offres")
+        title.setAlignment(Qt.AlignCenter)
+        title.setStyleSheet("font-size: 22px; font-weight: bold;")
+
+        search_input = QLineEdit()
+        search_input.setPlaceholderText("Rechercher un poste (ex: Administrateur Systèmes, DevOps...)")
+
+        btn_search = QPushButton("Lancer la recherche")
+
+        layout.addWidget(title)
+        layout.addSpacing(20)
+        layout.addWidget(search_input)
+        layout.addWidget(btn_search)
+        layout.addStretch()
