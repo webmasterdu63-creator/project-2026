1c2635bff464bd319a1d2b48435a8bb9e378d843
BOUCHERET <webmasterdu63@gmail.com>
2026-02-08 19:01:24 +0100

    Update indeed_api.py

--- indeed_api.py
+++ indeed_api.py
@@ -1,1 +1,11 @@
+def search_indeed(job, location, contract):
+    return [
+        {
+            "source": "Indeed",
+            "title": f"{job} - Indeed",
+            "location": location,
+            "contract": contract,
+            "url": "https://indeed.com"
+        }
+    ]
 
