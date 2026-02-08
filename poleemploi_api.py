15a0374988c36a4f6027545f5c084ee445ede711
BOUCHERET <webmasterdu63@gmail.com>
2026-02-08 19:02:32 +0100

    Update poleemploi_api.py

--- poleemploi_api.py
+++ poleemploi_api.py
@@ -1,1 +1,11 @@
 
+def search_pole_emploi(job, location, contract):
+    return [
+        {
+            "source": "Pôle Emploi",
+            "title": f"{job} - Pôle Emploi",
+            "location": location,
+            "contract": contract,
+            "url": "https://pole-emploi.fr"
+        }
+    ]
